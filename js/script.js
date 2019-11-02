0972f243693f4a2767a744eba3a373183d50d408
Jakub Jedynak <kuba7486@gmail.com>
2019-11-02 18:29:52 +0100

    Add game score section in script.js

--- js/script.js
+++ js/script.js
@@ -26,11 +26,42 @@ let playerMove = 'nieznany ruch';
 if(playerInput == '1'){
   playerMove = 'kamień';
 }
-else if(playerInput = '2'){
+else if(playerInput == '2'){
     playerMove = 'papier';
 }
-else if(playerInput = '3'){
+else if(playerInput == '3'){
     playerMove = 'nożyce'
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if(computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Gracz wygrywa!');
+  }
+  else if(computerMove =='kamień' && playerMove == 'nożyce'){
+      printMessage('Komputer wygrywa!');
+  }
+  else if(computerMove == 'kamień' && playerMove == 'kamień'){
+      printMessage('Nikt nie wygrywa!');
+  }
+  else if(computerMove == 'papier' && playerMove == 'nożyce'){
+      printMessage('Gracz wygrywa!');
+  }
+  else if(computerMove == 'papier' && playerMove == 'kamień'){
+      printMessage('Komputer wygrywa!');
+  }
+  else if(computerMove == 'papier' && playerMove == 'papier'){
+      printMessage('Nikt nie wygrywa!');
+  }
+  else if(computerMove == 'nożyce' && playerMove == 'kamień'){
+      printMessage('Gracz wygrywa!');
+  }
+  else if(computerMove == 'nożyce' && playerMove == 'papier'){
+      printMessage('Komputer wygrywa!');
+  }
+  else if(computerMove == 'nożyce' && playerMove == 'nożyce'){
+      printMessage('Nikt nie wygrywa!');
+  }
+  else if(playerMove == 'nieznany ruch'){
+      printMessage('Błąd');
+  }
